1104e196ace7ce71e019f77d80f331f9afcddcfe
Nate Stuyvesant <nate@spotlight.ai>
2022-04-10 12:36:26 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'carbon-angular-13';
+  data = [
+    {
+      group: '2V2N 9KYPM version 1',
+      value: 20000
+		},
+		{
+      group: 'L22I P66EP L22I P66EP L22I P66EP',
+      value: 65000
+		},
+		{
+      group: 'JQAI 2M4L1',
+      value: 75000
+		},
+		{
+      group: 'J9DZ F37AP',
+      value: 1200
+		},
+		{
+      group: 'YEL48 Q6XK YEL48',
+      value: 10000
+		},
+		{
+      group: 'Misc',
+      value: 25000
+		}
+  ]
+
+	options = {
+		title: 'Donut',
+		resizable: true,
+		donut: {
+				center: {
+						label: 'Browsers'
+				}
+		},
+		height: '400px'
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { ChartsModule } from "@carbon/charts-angular"
+import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
